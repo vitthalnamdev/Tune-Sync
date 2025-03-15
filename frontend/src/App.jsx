f8dc4bfc873b1145a8e337b2dc2350af37c8a33f
Vitthal Namdev <vitthal7804@gmail.com>
2025-03-15 13:50:15 +0530

    commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,35 +6,42 @@ import Signup from "./pages/Signup";
 import LoginForm from "./pages/Login";
 import VerifyEmail from "./pages/verify_Email";
 import Set_username from "./pages/Set_username";
-import { useDispatch } from "react-redux";
 import { Routes, Route } from "react-router-dom";
-import {fetchProfile} from "./services/operations/auth"
+import { fetchProfile } from "./services/operations/auth";
 
 function App() {
-  //localStorage.getItem("token");
-  // fetch all the songs from the database.
-  // then send that array , to Home to display it.
-  const [user , setuser] = useState({});
-  const [loading , setLoading] = useState(true);
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
   useEffect(() => {
     const getProfile = async () => {
       try {
-        const userData = await fetchProfile();
-        console.log(userData);
-        setuser(userData);
+        setTimeout(async () => {
+          const userData = await fetchProfile();
+          console.log("Fetched User:", userData);
+          setUser(userData);
+          setLoading(false);
+        }, 1500); 
       } catch (error) {
         console.error("Failed to fetch profile:", error);
-      } finally{
         setLoading(false);
       }
     };
     getProfile();
   }, []);
-  if (loading) return <div className="bg-gray-800 text-white flex justify-center items-center h-screen">Loading...</div>;
+
+  if (loading) {
+    return (
+      <div className="bg-gray-800 text-white flex justify-center items-center h-screen">
+        <div className="w-12 h-12 border-4 border-white border-t-transparent rounded-full animate-spin"></div>
+      </div>
+    );
+  }
+
   return (
     <div>
       <Routes>
-        <Route path="/" element={<Home user = {user}/>} />
+        <Route path="/" element={<Home user={user} />} />
         <Route path="/Signup" element={<Signup />} />
         <Route path="/Login" element={<LoginForm />} />
         <Route path="/Profile" element={<Profile />} />
