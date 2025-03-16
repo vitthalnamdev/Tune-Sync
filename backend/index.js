ed3e8228b2eae069954a06d8320ac9a354c7187f
mahendrajangid2583 <mahendrajangid2583@gmail.com>
2025-03-16 14:19:18 +0530

    commit

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ dbConnect();
 app.use(express.json());
 app.use(cookieParser());
 
-app.use(cors({origin:"https://oauth.pstmn.io/v1/browser-callback"}));
+app.use(cors());
 
 app.use(
     fileUpload({
@@ -25,7 +25,7 @@ app.use(
 const userRoutes = require("./routes/user");
 const songRoutes = require("./routes/song");
 
-app.use("/api/v1/auth",userRoutes);
+app.use("/api/v1",userRoutes);
 app.use("/api/v1/songs",songRoutes);
 // cloudinaryConnect();
 dbConnect();
